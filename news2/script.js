b8b12224a4e00d44b9813dfe86dbdbf98332f64c
gkaysofficial <87201354+gkaysofficial@users.noreply.github.com>
2021-07-12 10:58:36 -0700

    Update script.js

--- news2/script.js
+++ news2/script.js
@@ -12,13 +12,13 @@ xhr.onload = function() {
         let newshtml = "";
         articles.forEach(function(element, index) {
 
-            let news = ` <div class="accordion accordion-flush" id="accordionFlushExample">
+            let news = ` <div class="accordion accordion-flush" id="accordionFlushExample${index}">
             <h2 class="accordion-header" id="flush-headingOne">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapse${index}">
          ${element["title"]} <b>News Fetched by :-${element["author"]}</b>
         </button>
         </h2>
-        <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+        <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}" data-bs-parent="#accordionFlushExample">
             <div class="accordion-body">${element["description"]}. <a href="${element['url']}" target="_blank" >Read more here</a></div>
         </div>
     </div>`;
